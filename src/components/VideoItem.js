5889c2bf410bb52981ab66c4fd0669b047dae644
Alison Cordeiro <acordeiro@gmail.com>
2022-05-17 12:22:06 -0700

    Add callback for selected video

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,20 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({ id }) => {
-  return <div>Video Item: {id} </div>
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div onClick={() => onVideoSelect(video)} className='video-item item'>
+      <img
+        className='ui image'
+        alt={video.snippet.description}
+        src={video.snippet.thumbnails.medium.url}
+      />
+      <section className='content'>
+        <h1 className="header">{video.snippet.title}</h1>
+        <p className='description'>{video.snippet.description}</p>
+      </section>
+    </div>
+  );
 };
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,13 +2,14 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 // Destructuring is so we can say "videos" rather than "props.videos"
-const VideoList = ({ videos }) => {
-  const renderedList = videos.map(({ id }) => {
-    console.log(id.videoId);
-    return <VideoItem key={id.videoId} id={id.videoId} />
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    // console.log(video.id.videoId);
+    // console.log(video);
+    return <VideoItem onVideoSelect={onVideoSelect} key={video.id.videoId} video={video} />
   });
 
-  return <div>{renderedList}</div>
+  return <div className="ui relaxed divided list" >{renderedList}</div>
 };
 
 export default VideoList;
