c28757e5b687c6d6cc5ea069dc9aa8bac1ad3ca3
Alison Cordeiro <acordeiro@gmail.com>
2022-05-17 09:21:58 -0700

    Add stylesheet

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: 'Enter your search here' };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    // Make sure we call callback from parent component
+  }
+
   render() {
-    return <div>Search Bar!</div>
+    return (
+      // ui segment will draw a nice rectangle around everything inside it
+      <div className='search-bar ui segment'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
+          <div className='field'>
+            <label>Video Search</label>
+            <input
+              type='text'
+              value={this.state.term}
+              // DON'T DO THIS! IT WILL NOT HANDLE CORRECTLY
+              // onChange={this.setState(term)}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
